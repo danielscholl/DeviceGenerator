f91475632d6ccbd1159bcad7b018b1011615a08b
Daniel Scholl <daniel.scholl@microsoft.com>
2019-03-09 10:33:23 -0600

    Initial Code

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Extensions.Configuration;
+
+namespace DeviceGenerator
+{
+    static class Program
+    {
+        static async Task Main()
+        {
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var count = Convert.ToInt32(configuration.GetSection("DeviceCount").Value, CultureInfo.CurrentCulture);
+            var multiplier = Convert.ToInt32(configuration.GetSection("Multiplier").Value, CultureInfo.CurrentCulture);
+            var identityDb = RegistryManager.CreateFromConnectionString(configuration.GetConnectionString("Hub"));
+
+            for (int m = 1; m < multiplier + 1; m++)
+            {
+                var deviceList = new List<Device>();
+                for (int i = 1; i < count + 1; i++)
+                {
+                    var deviceId = Guid.NewGuid().ToString();
+                    //Console.WriteLine($"Add Device:  '{deviceId}'");
+                    await identityDb.AddDeviceAsync(new Device(deviceId)).ConfigureAwait(false);
+                    deviceList.Add(new Device(deviceId));                
+                }
+
+                await identityDb.AddDevices2Async(deviceList).ConfigureAwait(false);
+                Console.WriteLine($"Send Batch {m}: Completed");
+            }
+            Console.WriteLine($"Device Generation: {count * multiplier} Sent");
+            var registryStats = await identityDb.GetRegistryStatisticsAsync().ConfigureAwait(false);
+            Console.WriteLine($"Registry now has {registryStats.TotalDeviceCount} devices.");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Device Generator
+
+appsettings.json
+```js
+{
+    "DeviceCount": "100",
+    "Multiplier": "100",
+    "ConnectionStrings": {
+      "Hub": "<hub_connection_string>"
+    }
+}
+```
